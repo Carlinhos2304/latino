e0453f60a163c34e177d95629fee909d6f85ad2e
Azxgo <azugmoreira@gmail.com>
2023-11-30 21:35:08 -0300

    uwu

--- web/web/settings.py
+++ web/web/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-$!uo$$3jk+utvd_l+s%1=w&965$^5h5abetp#7x=2o^g0rpv2(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
