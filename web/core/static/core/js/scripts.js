02f6e2fa3369ba734976fa8a9049bf11d642bb44
Cuenta Inacap <marcelo.salamanca02@inacapmail.cl>
2023-12-02 00:16:10 -0300

    cambios testimonios

--- web/core/static/core/js/scripts.js
+++ web/core/static/core/js/scripts.js
@@ -9,3 +9,15 @@ $(document).ready(function() {
     });
 });
 // FIN
+
+//TESTIMONIOS.HTML CARRUSEL
+
+$(document).ready(function() {
+    // Inicia el carrusel de testimonios
+    $('#testimoniosCarousel').carousel();
+
+    // Configura la velocidad de cambio para el carrusel de testimonios (en milisegundos)
+    $('#testimoniosCarousel').carousel({
+        interval: 8000 // Cambiar cada 3 segundos
+    });
+});
